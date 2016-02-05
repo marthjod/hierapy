51747f1af71d5cc94ce1c7a607207aad5d069325
marthjod@gmail.com <marthjod@gmail.com>
2016-02-05 16:10:48 +0100

    KISS

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+import json
+
 from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor
 
@@ -5,50 +7,41 @@ from parsimonious.nodes import NodeVisitor
 class HieraOutputParser(NodeVisitor):
 
     grammar = """
-        value          = nil / int / string / array / hash
-        array          = empty_array / nonempty_array
-        empty_array    = open_bracket whitespace close_bracket
-        nonempty_array = open_bracket whitespace value (whitespace comma whitespace value)* whitespace close_bracket
-        hash           = empty_hash / nonempty_hash
-        empty_hash     = open_curly whitespace close_curly
-        nonempty_hash  = open_curly whitespace key whitespace arrow whitespace value whitespace close_curly
-        open_bracket   = "["
-        close_bracket  = "]"
-        open_curly     = "{"
-        close_curly    = "}"
+        input          = token*
+        token          = nil / arrow / quote / rest
         nil            = "nil"
-        whitespace     = " "*
-        comma          = whitespace* "," whitespace*
-        string         = ~"[A-Z 0-9]*"i
-        key            = string # alias
-        int            = ~"[0-9]+"
         arrow          = "=>"
+        quote          = '"'
+        rest           = ~r"[a-z 0-9\[\]\{\},]*"i
     """
 
     def __init__(self, grammar=None, text=None):
         self.grammar = grammar or HieraOutputParser.grammar
         ast = Grammar(self.grammar).parse(text)
-        self.result = None
+        self.result = []
         self.visit(ast)
 
-    def visit_empty_array(self, node, children):
-        self.result = []
-
-    def visit_nonempty_array(self, node, children):
-        print node
-        self.result = node.text
-
     def visit_nil(self, node, children):
-        self.result = None
+        self.result.append("null")
 
-    def visit_int(self, node, children):
-        try:
-            self.result = int(node.text)
-        except ValueError:
-            self.result = node.text
+    def visit_arrow(self, node, children):
+        self.result.append(":")
 
-    def visit_string(self, node, children):
-        self.result = node.text
+    def visit_quote(self, node, children):
+        self.result.append(node.text)
+
+    def visit_rest(self, node, children):
+        self.result.append(node.text)
 
     def generic_visit(self, node, children):
         pass
+
+    def get_json(self):
+        return "".join(self.result)
+
+    def get_python(self):
+        j = self.get_json()
+        try:
+            return json.loads(j)
+        except ValueError:
+            return j
--- test_parser.py
+++ test_parser.py
@@ -4,10 +4,36 @@ from parser import HieraOutputParser
 
 
 def main():
-    assert HieraOutputParser(text='''nil''').result is None
-    assert HieraOutputParser(text='''9''').result == 9
-    assert HieraOutputParser(text='''some string''').result == "some string"
-    assert HieraOutputParser(text='''[]''').result == []
+    assert HieraOutputParser(text="nil").get_json() == "null"
+    assert HieraOutputParser(text="nil").get_python() is None
+
+    assert HieraOutputParser(text="=>").get_json() == ":"
+    assert HieraOutputParser(text="=>").get_python() == ":"
+
+    assert HieraOutputParser(text="some string").get_json() == "some string"
+    assert HieraOutputParser(text="some string").get_python() == "some string"
+
+    assert HieraOutputParser(text="[]").get_json() == "[]"
+    assert HieraOutputParser(text="[]").get_python() == []
+
+    assert HieraOutputParser(text="{}").get_json() == "{}"
+    assert HieraOutputParser(text="{}").get_python() == {}
+
+    assert HieraOutputParser(text='{"foo"=>"bar"}').get_json() == '{"foo":"bar"}'
+    assert HieraOutputParser(text='{"foo"=>"bar"}').get_python() == {"foo":"bar"}
+
+    assert HieraOutputParser(text='{"foo" => "bar"}').get_json() == '{"foo" : "bar"}'
+    assert HieraOutputParser(text='{"foo" => "bar"}').get_python() == {"foo":"bar"}
+
+    assert HieraOutputParser(text='{"foo"=>[1,2]}').get_json() == '{"foo":[1,2]}'
+    assert HieraOutputParser(text='{"foo"=>[1,2]}').get_python() == {"foo": [1, 2]}
+
+    assert HieraOutputParser(text='{"foo"=>nil}').get_json() == '{"foo":null}'
+    assert HieraOutputParser(text='{"foo"=>nil}').get_python() == {"foo": None}
+
+    assert HieraOutputParser(text='{"foo"=>[{}, 3]}').get_json() == '{"foo":[{}, 3]}'
+    assert HieraOutputParser(text='{"foo"=>[{}, 3]}').get_python() == {"foo": [{}, 3]}
+
 
 if __name__ == "__main__":
     main()
